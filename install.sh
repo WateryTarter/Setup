1791c7a6292c411081b8fff0e7278cd60d02be2f
WateryTarter <peter.cummin96@gmail.com>
2026-04-13 15:11:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,14 @@
 
 IPs=("172.24.19.44" "172.24.19.68" "172.24.19.82" "172.24.18.30" "172.24.21.12" "172.24.22.19" "172.24.10.180" "172.24.10.1" "172.24.19.1" "172.24.22.1")
 USER="aliyev.sahrab"
-URL="http://10.50.160.33/tmp/scripts/comb.sh"
-PATH="/tmp/install.sh"
+URL="http://10.50.160.33:8000/comb.sh"
+L_PATH="/tmp/install.sh"
 
 for target in "${IPs[@]}"; do
 	echo "--- Starting Deployment to $target ---"
-	ssh "$USER@$target" "wget -q $URL -O $PATH && \
-		chmod +x $PATH && \
-		./$PATH" 2> /tmp/remote_error.log && \
-		rm "$PATH"
+	ssh "$USER@$target" "wget -q $URL -O $L_PATH && \
+		chmod +x $L_PATH && \
+		sudo $L_PATH && rm $L_PATH" 2> /tmp/remote_error.log
 
 	if [ $? -ne 0 ]; then
 		echo "FAILED: Stopped at $target"
